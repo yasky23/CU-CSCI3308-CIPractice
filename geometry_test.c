6d03dcf276f938afaa04a08d53fcc4eacc049917
Lukasz <lust1393@colorado.edu>
2015-12-02 17:54:29 -0700

    rec2

--- geometry_test.c
+++ geometry_test.c
@@ -14,6 +14,23 @@
 #include <check.h>
 
 #include "geometry.h"
+/* Test for the area */
+START_TEST(test_2d_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+   
+    a.x = 0;
+    a.y = 0;
+    b.x = 0;
+    b.y = 4;
+    c.x = 4;
+    c.y = 0;
+    
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 8);
+}
+END_TEST
 
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
@@ -162,10 +179,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
